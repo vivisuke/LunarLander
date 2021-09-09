603fdaa85bcbdf77cfa40cd547242be976bc6acd
vivisuke <ntsuda@master.email.ne.jp>
2021-09-10 08:53:33 +0900

    0005 ◎ C 09/10 09/10 0.001│ToDo│上部に着陸船速度表示

--- LunaLander/MainScene.gd
+++ LunaLander/MainScene.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
-const ACCEL_GY = 0.2		# 重力加速度
+const ACCEL_GY = 1.0		# 重力加速度
 
 var velocity = Vector2(0, 10)
+
+var Explosion = load("res://Explosion.tscn")
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity += Vector2(0, ACCEL_GY)
-	velocity = $LunaModule.move_and_slide(velocity)
+	velocity += Vector2(0, ACCEL_GY)*delta
+	var v0 = velocity
+	velocity = $LunaModule.move_and_slide(velocity, Vector2.UP)
+	$InfoBar/SpeedLabel.text = "Speed: %f, %f" % [velocity.x, velocity.y]
+	print($LunaModule.is_on_floor())
+	if $LunaModule.is_on_floor() && v0.y >= 10:
+		$LunaModule.hide()
+		var ex = Explosion.instance()
+		ex.position = $LunaModule.position
+		add_child(ex)
+		ex.restart()
