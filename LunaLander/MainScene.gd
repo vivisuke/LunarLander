6b73aad93520b3769a10c1f2c2329de708ae3d0e
vivisuke <ntsuda@master.email.ne.jp>
2021-09-10 10:15:17 +0900

    0008 ◎ C 09/10 09/10 0.001│ToDo│上部に着陸船残燃料表示 0009 ◎ C 09/10 09/10 0.001│ToDo│右上に 1Round ボタン設置

--- LunaLander/MainScene.gd
+++ LunaLander/MainScene.gd
@@ -3,19 +3,29 @@ extends Node2D
 const SCREEN_HEIGHT = 640
 const ACCEL_GY = 1.0		# 重力加速度
 const ACCEL_IY = 5.0		# メインエンジン噴射加速度
+const FULL_FUEL = 5000		# 燃料量初期値
+const CONSUMPTION = 10		# メインエンジン噴射燃料消費量
+const LANDING_MAX_SPEED = 5.0	# 着陸許容速度
 
 var velocity = Vector2(0, 10)
+var fuel = FULL_FUEL# 燃料残量
 
 var Explosion = load("res://Explosion.tscn")
 
+func updateFuel():
+	$InfoBar/FuelLabel.text = "FUEL: %d" % fuel
 func _ready():
+	updateFuel()
 	pass # Replace with function body.
 
 func _physics_process(delta):
 	velocity += Vector2(0, ACCEL_GY)*delta
-	if Input.is_action_pressed("ui_up"):	# ↑キー押下時
-		velocity -= Vector2(0, ACCEL_IY)*delta
-		$LunaModule/AnimatedSprite.play("injection")
+	if( Input.is_action_pressed("ui_up") &&		# ↑キー押下時
+		fuel >= CONSUMPTION && !$LunaModule.is_on_floor()):		# 燃料あり && 着陸していない
+			fuel -= CONSUMPTION
+			updateFuel()
+			velocity -= Vector2(0, ACCEL_IY)*delta
+			$LunaModule/AnimatedSprite.play("injection")
 	else:
 		$LunaModule/AnimatedSprite.play("default")
 	var v0 = velocity
@@ -23,9 +33,13 @@ func _physics_process(delta):
 	$InfoBar/SpeedLabel.text = "Speed: %.3f, %.3f" % [velocity.x, velocity.y]
 	$InfoBar/PosLabel.text = "Pos: %.3f, %.3f" % [$LunaModule.position.x, SCREEN_HEIGHT - $LunaModule.position.y]
 	#print($LunaModule.is_on_floor())
-	if $LunaModule.is_on_floor() && v0.y >= 10:
+	if $LunaModule.is_on_floor() && v0.y >= LANDING_MAX_SPEED:
 		$LunaModule.hide()
 		var ex = Explosion.instance()
 		ex.position = $LunaModule.position
 		add_child(ex)
 		ex.restart()
+
+
+func _on_1RoundButton_pressed():
+	pass # Replace with function body.
